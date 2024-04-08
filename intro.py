16aa87b92e4f30740f65d684f0856df8a03c9dfa
Joseph Fansen Kong <joseph.kong@planetargon.com>
2024-04-08 16:28:29 -0700

    python syntax intro

---
+++ intro.py
@@ -0,0 +1,19 @@
+print("Joseph Kong")
+print('o---')
+print(' ||||')
+print('*' * 10)
+
+price = 10 
+price = 20
+rate = 4.9
+is_published = True
+print (price)
+print (rate)
+print (is_published)
+
+name ='John Smith'
+age = 20 
+is_new = True
+name = input('What is your name? ')
+color = input('And what is your favroite color? ')
+print( name +' likes '+ color )
