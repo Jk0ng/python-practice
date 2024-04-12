0f8e0705018e40cb530e634079fc442815f9388d
Joseph Fansen Kong <joseph.kong@planetargon.com>
2024-04-11 21:46:11 -0700

    61a lecture higher-order functions

--- conditional.py
+++ conditional.py
@@ -1,25 +1,31 @@
+
 def absolute_value(x): 
-    """Return the absolute value of x"""
-    if x > 0:
+    """The function to return the absolute value of an number, the number could be +/- or zero"""
+    if x == 0:
+        return 0 
+    elif x > 0:
         return x
-    elif x < 0:
-        return -x
+    elif x < 0: 
+        return - 0 
     else: 
-        return 0
+        return 'error'
     
 print(absolute_value(-9))
 print(absolute_value(9))
 print(absolute_value(0))
 
-def fib(n):
-    """Find the Nth fibonacci number"""
-    prev, curr=0,1
-    k = 1 
+def fibonacci(n): 
+    """find the number at nth fibonacci sequence"""
+    prev, curr = 1,0
+    k = 1    
     while k < n:
-        prev, curr= curr, curr + prev
+        prev, curr = curr, prev + curr
         k = k + 1
-    return curr 
-print(fib(5))
-print(fib(6))
-print(fib(7))
-print(fib(8))
+    return curr
+    
+
+print(fibonacci(6))
+print(fibonacci(7))
+print(fibonacci(8))
+print(fibonacci(0))
+
