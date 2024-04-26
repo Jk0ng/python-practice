0a5e27920662e9101d15dd58110f93d511cdea32
Joseph Fansen Kong <joseph.kong@planetargon.com>
2024-04-25 21:12:01 -0700

    dictionary practice problem sets

--- my_dictionary.py
+++ my_dictionary.py
@@ -1,18 +1,18 @@
 student = {'name': 'Joseph', 'age': 25, 'courses': ['Math', 'CompSci'] }
 
 student['phone'] = '555-555-5555'
-print(student)
+# print(student)
 student.update({'name': 'Joseph Kong', 'age': 26}) # update several k:v pairs at the same time
 
-print(len(student))
-print(student.keys())
-print(student.values())
+# print(len(student))
+# print(student.keys())
+# print(student.values())
 print(student.items())
 
-for key, value in student.items():
+# for key, value in student.items():
    
-    print("This is the %s: %s pair " % (key, value)) #modulo string formatting
-    print(f'This is the {key} and {value}')  # f string string-interpolation
+#     print("This is the %s: %s pair " % (key, value)) #modulo string formatting
+#     print(f'This is the {key} and {value}')  # f string string-interpolation
 
 
 
@@ -24,3 +24,10 @@ course_taking = student.pop('courses') # use a .pop() method to remove a key:val
 # print(student.get('name'))
 # print(student.get('phone', 'Not Found'))
 
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")
